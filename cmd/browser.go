7781caaeddedbd882d68f9dff9f737ed60b25638
Alex Zhao <git@zhaochunqi.com>
2025-10-27 12:24:05 +0800

    use xdg-open on linux

--- cmd/browser.go
+++ cmd/browser.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"errors"
+	"os/exec"
+	"runtime"
 
 	"github.com/pkg/browser"
 )
@@ -10,7 +12,24 @@ import (
 var ErrMockBrowser = errors.New("mock browser error")
 
 // OpenURLInBrowser is exported for testing
-var OpenURLInBrowser = browser.OpenURL
+var OpenURLInBrowser = openURLInBrowser
+
+func openURLInBrowser(url string) error {
+	// On Linux, use xdg-open with output redirection to suppress messages
+	if runtime.GOOS == "linux" {
+		return openWithXdgOpen(url)
+	}
+	// For other platforms, use the default browser library
+	return browser.OpenURL(url)
+}
+
+func openWithXdgOpen(url string) error {
+	cmd := exec.Command("xdg-open", url)
+	// Redirect stdout and stderr to /dev/null to suppress output
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	return cmd.Start()
+}
 
 func openURLInBrowserFunc(url string) error {
 	return OpenURLInBrowser(url)
