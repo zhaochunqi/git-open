d51a9251da0d3848084fb3c2e9e7aa5193f78faf
Alex Zhao <git@zhaochunqi.com>
2025-10-27 12:59:50 +0800

    update open function

--- cmd/browser_test.go
+++ cmd/browser_test.go
@@ -83,3 +83,189 @@ func Test_openWithXdgOpen(t *testing.T) {
 		})
 	}
 }
+
+func Test_openWithMacOSOpen(t *testing.T) {
+	// This test is for macOS-specific functionality
+	if runtime.GOOS != "darwin" {
+		t.Skip("Skipping test on non-macOS platforms")
+	}
+
+	tests := []struct {
+		name    string
+		url     string
+		wantErr bool
+	}{
+		{
+			name:    "success",
+			url:     "https://github.com/zhaochunqi/git-open",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := openWithMacOSOpen(tt.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("openWithMacOSOpen() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_openURLInBrowser_PlatformSpecific(t *testing.T) {
+	// Save original function
+	original := OpenURLInBrowser
+	defer func() {
+		OpenURLInBrowser = original
+	}()
+
+	tests := []struct {
+		name        string
+		url         string
+		expectError bool
+	}{
+		{
+			name:        "valid URL",
+			url:         "https://github.com/zhaochunqi/git-open",
+			expectError: false,
+		},
+		{
+			name:        "empty URL",
+			url:         "",
+			expectError: false, // Commands might handle empty URL gracefully
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Test the actual platform-specific implementation
+			err := openURLInBrowser(tt.url)
+			if (err != nil) != tt.expectError {
+				t.Errorf("openURLInBrowser() error = %v, expectError %v", err, tt.expectError)
+			}
+		})
+	}
+}
+
+func Test_openURLInBrowser_AllPlatforms(t *testing.T) {
+	tests := []struct {
+		name     string
+		url      string
+		platform string
+	}{
+		{
+			name:     "Linux platform",
+			url:      "https://github.com/zhaochunqi/git-open",
+			platform: "linux",
+		},
+		{
+			name:     "macOS platform",
+			url:      "https://github.com/zhaochunqi/git-open",
+			platform: "darwin",
+		},
+		{
+			name:     "Windows platform",
+			url:      "https://github.com/zhaochunqi/git-open",
+			platform: "windows",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Save the original runtime.GOOS
+			originalGOOS := runtime.GOOS
+			// We can't actually change runtime.GOOS, but we can test the functions directly
+			
+			switch tt.platform {
+			case "linux":
+				// Test openWithXdgOpen directly if not on Linux
+				if runtime.GOOS != "linux" {
+					err := openWithXdgOpen(tt.url)
+					// On non-Linux systems, this should fail as xdg-open doesn't exist
+					if err == nil {
+						t.Logf("openWithXdgOpen() succeeded on %s platform, command might exist", originalGOOS)
+					}
+				}
+			case "darwin":
+				// Test openWithMacOSOpen directly if not on macOS
+				if runtime.GOOS != "darwin" {
+					err := openWithMacOSOpen(tt.url)
+					// On non-macOS systems, this should fail as open command might not exist
+					if err == nil {
+						t.Logf("openWithMacOSOpen() succeeded on %s platform, command might exist", originalGOOS)
+					}
+				}
+			case "windows":
+				// Test openWithWindowsStart directly if not on Windows
+				if runtime.GOOS != "windows" {
+					err := openWithWindowsStart(tt.url)
+					// On non-Windows systems, this should fail as cmd doesn't exist
+					if err == nil {
+						t.Logf("openWithWindowsStart() succeeded on %s platform, command might exist", originalGOOS)
+					}
+				}
+			}
+		})
+	}
+}
+
+func Test_openWithWindowsStart(t *testing.T) {
+	// This test is for Windows-specific functionality
+	if runtime.GOOS != "windows" {
+		t.Skip("Skipping test on non-Windows platforms")
+	}
+
+	tests := []struct {
+		name    string
+		url     string
+		wantErr bool
+	}{
+		{
+			name:    "success",
+			url:     "https://github.com/zhaochunqi/git-open",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := openWithWindowsStart(tt.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("openWithWindowsStart() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_openURLInBrowser_UnsupportedPlatform(t *testing.T) {
+	// Test error handling for unsupported platforms
+	// We can't actually test this directly since we can't change runtime.GOOS,
+	// but we can test the error message format
+	url := "https://github.com/zhaochunqi/git-open"
+	
+	// Create a mock scenario by testing the current platform
+	// This test primarily ensures the function structure is correct
+	err := openURLInBrowser(url)
+	
+	// On supported platforms (linux, darwin, windows), this should succeed
+	// On unsupported platforms, this would return an error
+	supportedPlatforms := map[string]bool{
+		"linux":   true,
+		"darwin":  true,
+		"windows": true,
+	}
+	
+	if supportedPlatforms[runtime.GOOS] {
+		// Current platform is supported, so error should be nil or a command execution error
+		if err != nil {
+			t.Logf("openURLInBrowser() error on supported platform %s: %v (this might be expected in CI)", runtime.GOOS, err)
+		}
+	} else {
+		// Current platform is not supported, should return unsupported platform error
+		if err == nil {
+			t.Errorf("openURLInBrowser() should return error for unsupported platform %s", runtime.GOOS)
+		} else if err.Error() != "unsupported platform: "+runtime.GOOS {
+			t.Errorf("openURLInBrowser() error = %v, want 'unsupported platform: %s'", err, runtime.GOOS)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -7,10 +7,7 @@ require (
 	github.com/spf13/viper v1.20.1
 )
 
-require (
-	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-)
+require github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
@@ -24,17 +21,12 @@ require (
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-git/go-git/v5 v5.16.3
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pjbgf/sha1cd v0.3.2 // indirect
-	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -46,13 +38,9 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/mod v0.19.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
-	golang.org/x/tools v0.23.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
